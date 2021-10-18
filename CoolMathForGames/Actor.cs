ddb716cb8ca23b36059773ce522d844c334e0959
WillMon23 <88592707+WillMon23@users.noreply.github.com>
2021-10-18 15:20:29 -0500

    Initialize Raylib

---
+++ CoolMathForGames/Actor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace CoolMathForGames
+{
+    struct Icon
+    {
+        public char Symbol;
+        public Color Color;
+    }
+    class Actor
+    {
+        private Icon _icon;
+        private string _name;
+        private Vector2 _position;
+        private bool _started;
+
+        /// <summary>
+        /// True if the start function has been called for this actor
+        /// </summary>
+        public bool Started { get { return _started; } }
+
+        public Vector2 Posistion { get { return _position; } set { _position = value; } }
+        
+        public Icon Icon { get { return _icon; } }
+
+        public string Name { get { return _name; } }
+
+        public Actor(char icon, Vector2 position, Color color, string name = "Actor")
+        {
+            _icon = new Icon { Symbol = icon, Color = color }; 
+            _name = name;
+            _position = position;
+        }
+
+        public Actor(char icon, float x, float y, Color color, string name = "Actor") :
+            this(icon, new Vector2 { X = x, Y = y }, color, name){ }
+
+        public virtual void Start()
+        {
+            _started = true;
+        }
+
+        public virtual void Update(float deltaTime)
+        {
+            Console.WriteLine(Name + ": " + Posistion.X + " , " + Posistion.Y);
+        }
+
+        public virtual void Draw()
+        {
+            Raylib.DrawText(Icon.Symbol.ToString(), (int)Posistion.X, (int)Posistion.Y, 50, Icon.Color);
+        }
+
+        public virtual void End()
+        {
+
+        }
+
+        public virtual void OnCollision( Actor actor)
+        {
+            Engine.CloseApplication();
+        }
+
+
+    }
+}
---
+++ CoolMathForGames/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace CoolMathForGames
+{
+    class Player : Actor
+    {
+        private float _speed;
+        private Vector2 _volocity;
+        
+        public float Speed { get { return _speed; } set { _speed = value; } }
+
+        public Vector2 Volocity {  get { return _volocity; } set { _volocity = value; } }
+
+        public Player(char icon, float x, float y, float speed, Color color, string name = "Actor") 
+            :base( icon,  x,  y,color,  name = "Actor"  )
+        {
+            _speed = speed;
+            
+        }
+
+        public override void Update(float deltaTime)
+        {
+
+            int xDirection = -Convert.ToInt32(Raylib.IsKeyDown(KeyboardKey.KEY_A)) 
+                + Convert.ToInt32(Raylib.IsKeyDown(KeyboardKey.KEY_D));
+
+            int yDirection = -Convert.ToInt32(Raylib.IsKeyDown(KeyboardKey.KEY_W)) + 
+                Convert.ToInt32(Raylib.IsKeyDown(KeyboardKey.KEY_S));
+
+            Vector2 moveDirecton = new Vector2(xDirection, yDirection);
+
+            Volocity =  moveDirecton.Normalzed * Speed * deltaTime ;
+
+            Posistion += Volocity;
+
+            base.Update(deltaTime);
+
+        }
+
+        public override void OnCollision(Actor actor)
+        {
+            Console.WriteLine("Collision Occured");
+        }
+
+
+    }
+}
